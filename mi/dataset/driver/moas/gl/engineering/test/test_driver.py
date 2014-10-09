e4f4307fb997591733ddd924a43cae3c9e90a01b
rhan <rhan1498@gmail.com>
2014-10-09 16:29:29 -0700

    Removed pyon import

--- mi/dataset/driver/moas/gl/engineering/test/test_driver.py
+++ mi/dataset/driver/moas/gl/engineering/test/test_driver.py
@@ -19,7 +19,7 @@ import unittest
 
 from nose.plugins.attrib import attr
 
-from pyon.agent.agent import ResourceAgentState
+from mi.core.instrument.instrument_driver import ResourceAgentState
 from interface.objects import ResourceAgentErrorEvent
 from exceptions import Exception
 
