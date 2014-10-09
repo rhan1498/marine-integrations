aceec1065d779485a2fcf7b512a1138964e2c5ca
rhan <rhan1498@gmail.com>
2014-10-09 14:54:34 -0700

    Removed pyon import

--- mi/dataset/driver/mflm/flort/test/test_driver.py
+++ mi/dataset/driver/mflm/flort/test/test_driver.py
@@ -22,7 +22,7 @@ import shutil
 from nose.plugins.attrib import attr
 from mock import Mock
 
-from pyon.agent.agent import ResourceAgentState
+from mi.core.instrument.instrument_driver import ResourceAgentState
 from interface.objects import ResourceAgentErrorEvent
 from interface.objects import ResourceAgentConnectionLostErrorEvent
 
